5009a4c351823d125be84cdeab57615e140c29a2
Dukewendo <dommct@yahoo.co.uk>
2023-09-17 22:58:52 +0100

    card link

--- src/components/Allcards.jsx
+++ src/components/Allcards.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-
 import "../components/allcards.css";
 
 export default function Allcards(props) {
-
   const cardData = props.cardData;
 
   return (
     <div className="card-container">
       {cardData.map((item) => (
-        <div className="card-content" key={item.cardtitle}>
-          <a href={`/${item.id}`}>View card</a>
-          <h3 className="card-title" >
-            Card Title: {item.cardtitle}
-          </h3>
-          <h4 className="card-category">Category: {item.category}</h4>
-          <h5 className="card-note">Notes: {item.cardnote}</h5>
-        </div>
+        <>
+          <a className="card-link" href={`/${item.id}`}>
+            <div className="card-content" key={item.cardtitle}>
+              <h3 className="card-title">Card Title: {item.cardtitle}</h3>
+              <h4 className="card-category">Category: {item.category}</h4>
+              <h5 className="card-note">Notes: {item.cardnote}</h5>
+            </div>
+          </a>
+        </>
       ))}
     </div>
   );
