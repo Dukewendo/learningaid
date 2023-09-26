6f3b291db5a94212e2e73263fce63ba981a5d8cb
Dukewendo <dommct@yahoo.co.uk>
2023-09-26 22:09:38 +0100

    using localstorage

--- src/App.js
+++ src/App.js
@@ -9,9 +9,20 @@ import Singlecard from "./components/Singlecard";
 import Newcard from "./components/Newcard";
 
 //data  swap to localstorage
-import cardData from "./components/mockdata.json";
+//import cardData from "./components/mockdata.json";
 
 function App() {
+  
+ let cardData = [];
+
+ cardData.push(JSON.parse(localStorage.getItem('session')));
+ localStorage.setItem('session', JSON.stringify(cardData));
+ 
+
+  //let cardData = [{"id": "0", "cardtitle": "Testcard1", "category": "Testcategory1", "cardnote": "test notes"}];
+
+
+  
   return (
     <BrowserRouter>
       <Routes>
