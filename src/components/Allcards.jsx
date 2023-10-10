0b11cb7d857b058853c3dab43441ea5ce27a2aff
Dukewendo <dommct@yahoo.co.uk>
2023-10-10 17:47:43 +0100

    fixed search

--- src/components/Allcards.jsx
+++ src/components/Allcards.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import "../components/allcards.css";
 
-export default function Allcards() {
-  let cardData = JSON.parse(localStorage.getItem("session")) || [];
-  
+export default function Allcards(props) {
+
+  const cardData = props.cardData;
 
   return (
     <div className="card-container">
       {cardData.map((item) => (
-        <>
-          <a className="card-link" href={`learninghome/#${item.cardtitle}`}>
-            <div className="card-content" key={item.cardtitle}>
-              <h3 className="card-title">Card Title: {item.cardtitle}</h3>
-              <h5 className="card-category">Category: {item.cardcategory}</h5>
-              <h5 className="card-note">Notes: {item.cardnote}</h5>
-            </div>
-          </a>
-        </>
+        <a className="card-link" href={`learninghome/#${item.cardtitle}`} key={item.cardtitle}>
+          <div className="card-content">
+            <h3 className="card-title">Card Title: {item.cardtitle}</h3>
+            <h5 className="card-category">Category: {item.cardcategory}</h5>
+            <h5 className="card-note">Notes: {item.cardnote}</h5>
+          </div>
+        </a>
       ))}
     </div>
   );
